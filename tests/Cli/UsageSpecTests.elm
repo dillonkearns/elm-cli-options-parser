b2352ccf93fff875d398106ef909c50963526c53
Dillon Kearns <dillon@dillonkearns.com>
2018-04-03 12:53:41 -0700

    Extract synopsis tests to UsageSpecTests suite.

---
+++ tests/Cli/UsageSpecTests.elm
@@ -0,0 +1,80 @@
+module Cli.UsageSpecTests exposing (all)
+
+import Command
+import Expect exposing (Expectation)
+import Test exposing (..)
+
+
+type Msg
+    = Help
+    | Version
+    | OpenUrl String
+    | OpenUrlWithFlag String Bool
+    | Name String
+    | FullName String String
+
+
+all : Test
+all =
+    describe "synopsis"
+        [ test "synopsis prints options with arguments" <|
+            \() ->
+                (Command.build FullName
+                    |> Command.optionWithStringArg "first-name"
+                    |> Command.optionWithStringArg "last-name"
+                    |> Command.toCommand
+                )
+                    |> Command.synopsis "greet"
+                    |> Expect.equal "greet --first-name <first-name> --last-name <last-name>"
+        , test "print synopsis with required flag" <|
+            \() ->
+                Command.build Version
+                    |> Command.expectFlag "version"
+                    |> Command.toCommand
+                    |> Command.synopsis "greet"
+                    |> Expect.equal "greet --version"
+        , test "print synopsis with optional arg" <|
+            \() ->
+                Command.build (,)
+                    |> Command.optionWithStringArg "name"
+                    |> Command.optionalOptionWithStringArg "prefix"
+                    |> Command.toCommand
+                    |> Command.synopsis "greet"
+                    |> Expect.equal "greet --name <name> [--prefix <prefix>]"
+        , test "print synopsis with required operand" <|
+            \() ->
+                Command.build identity
+                    |> Command.expectOperand "MyApp.elm"
+                    |> Command.toCommand
+                    |> Command.synopsis "elm-interop"
+                    |> Expect.equal "elm-interop <MyApp.elm>"
+        , test "print synopsis with doc string" <|
+            \() ->
+                Command.buildWithDoc (,) "greets somebody in your terminal"
+                    |> Command.optionWithStringArg "name"
+                    |> Command.optionalOptionWithStringArg "prefix"
+                    |> Command.toCommand
+                    |> Command.synopsis "greet"
+                    |> Expect.equal "greet --name <name> [--prefix <prefix>] # greets somebody in your terminal"
+        , test "print synopsis with zero or more arg option" <|
+            \() ->
+                (Command.build identity
+                    |> Command.zeroOrMoreWithStringArg "header"
+                )
+                    |> Command.toCommand
+                    |> Command.synopsis "curl"
+                    |> Expect.equal "curl [--header <header>]..."
+        , test "print rest operands synopsis" <|
+            \() ->
+                Command.build identity
+                    |> Command.captureRestOperands "files"
+                    |> Command.synopsis "rm"
+                    |> Expect.equal "rm <files>..."
+        , test "prints rest args at the end of the synopsis" <|
+            \() ->
+                Command.build (,)
+                    |> Command.withFlag "dry-run"
+                    |> Command.captureRestOperands "files"
+                    |> Command.synopsis "rm"
+                    |> Expect.equal "rm [--dry-run] <files>..."
+        ]
