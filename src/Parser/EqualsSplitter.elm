0f9f9a9a6b1062778c8af08ad9e1000c8dedb0e3
Dillon Kearns <dillon@dillonkearns.com>
2018-04-09 18:34:32 -0700

    Add EqualsSplitter module to help with parsing.

---
+++ src/Parser/EqualsSplitter.elm
@@ -0,0 +1,28 @@
+module Parser.EqualsSplitter exposing (SplitResult(..), split)
+
+
+type SplitResult
+    = Option String
+    | OptionWithArg { name : String, value : String }
+    | NotOption
+
+
+split : String -> SplitResult
+split string =
+    case String.toList string of
+        '-' :: '-' :: optionName ->
+            case String.split "=" (optionName |> String.fromList) of
+                [ optionName ] ->
+                    optionName
+                        |> Option
+
+                optionName :: splitAfterOptionName ->
+                    OptionWithArg { name = "name", value = String.concat splitAfterOptionName }
+
+                _ ->
+                    optionName
+                        |> String.fromList
+                        |> Option
+
+        _ ->
+            NotOption
---
+++ tests/Parser/EqualsSplitterTests.elm
@@ -0,0 +1,38 @@
+module Parser.EqualsSplitterTests exposing (all)
+
+import Expect exposing (Expectation)
+import Parser.EqualsSplitter as EqualsSplitter
+import Test exposing (..)
+
+
+type Msg
+    = Help
+    | Version
+    | OpenUrl String
+    | OpenUrlWithFlag String Bool
+    | Name String
+    | FullName String String
+
+
+all : Test
+all =
+    describe "equals splitter"
+        [ test "not option" <|
+            \() ->
+                "operand"
+                    |> EqualsSplitter.split
+                    |> Expect.equal EqualsSplitter.NotOption
+        , test "option without arg" <|
+            \() ->
+                "--name"
+                    |> EqualsSplitter.split
+                    |> Expect.equal (EqualsSplitter.Option "name")
+        , test "option with arg" <|
+            \() ->
+                "--name=Picard"
+                    |> EqualsSplitter.split
+                    |> Expect.equal
+                        (EqualsSplitter.OptionWithArg
+                            { name = "name", value = "Picard" }
+                        )
+        ]
