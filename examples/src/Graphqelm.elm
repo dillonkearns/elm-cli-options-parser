9572fba1a721ef8cfcb347723d2565f89a51ff9e
Dillon Kearns <dillon@dillonkearns.com>
2018-06-09 19:46:56 -0700

    Fix display name in graphql example.

--- examples/src/Graphqelm.elm
+++ examples/src/Graphqelm.elm
@@ -66,7 +66,7 @@ init flags =
             case matchResult of
                 Cli.NoMatch ->
                     "\nNo matching command...\n\nUsage:\n\n"
-                        ++ Cli.helpText "simple" cli
+                        ++ Cli.helpText "graphqelm" cli
 
                 Cli.ValidationErrors validationErrors ->
                     "Validation errors:\n\n"
