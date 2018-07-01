951d3396d9ecb53f5f5260cd2d861eb288687c84
Dillon Kearns <dillon@dillonkearns.com>
2018-07-01 14:51:54 -0700

    Add Cli test for match.

---
+++ tests/CliTests.elm
@@ -0,0 +1,23 @@
+module CliTests exposing (all)
+
+import Cli
+import Cli.Command as Command
+import Expect exposing (Expectation)
+import Test exposing (..)
+
+
+all : Test
+all =
+    describe "synopsis"
+        [ test "matching command" <|
+            \() ->
+                let
+                    cli =
+                        [ Command.build 123
+                            |> Command.expectFlag "help"
+                            |> Command.toCommand
+                        ]
+                in
+                Cli.try cli [ "", "", "--help" ]
+                    |> Expect.equal (Cli.Match 123)
+        ]
