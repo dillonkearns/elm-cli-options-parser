e4edf95fe958c4c7492adcda95acb1e41708a536
Dillon Kearns <dillon@dillonkearns.com>
2018-07-31 15:29:49 -0700

    Include xhr2 npm package to workaround Elm error.

--- examples/src/elm-test.js
+++ examples/src/elm-test.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 console.log(__filename)
 let program = require('./elm.js').Main.worker(process.argv)
-XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest
+XMLHttpRequest = require('xhr2')
 
 program.ports.print.subscribe(message => console.log(message))
 program.ports.printAndExitFailure.subscribe(message => {
