d09fab45b0b2ba16e2ccbe2ad6e21e76cdffe492
Dillon Kearns <dillon@dillonkearns.com>
2018-08-07 16:05:30 -0700

    Expose BuilderState module.

--- src/Cli/OptionsParser/BuilderState.elm
+++ src/Cli/OptionsParser/BuilderState.elm
@@ -1,13 +1,25 @@
 module Cli.OptionsParser.BuilderState exposing (AnyOptions, EndOptionsOnly, Terminal)
 
+{-|
 
+@docs AnyOptions, EndOptionsOnly, Terminal
+
+-}
+
+
+{-| A state where you can add any options (beginning, middle, or terminal)
+-}
 type AnyOptions
     = AnyOptions
 
 
+{-| A state where you can add anything but beginning options (i.e. middle or terminal)
+-}
 type EndOptionsOnly
     = EndOptionsOnly
 
 
+{-| A state where you can no longer add any options
+-}
 type Terminal
     = Terminal
