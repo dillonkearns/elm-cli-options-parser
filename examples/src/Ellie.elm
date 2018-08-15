4b511aef7915e9506f0b2285888ee46c60aae03c
Dillon Kearns <dillon@dillonkearns.com>
2018-08-14 22:55:50 -0700

    Use git example for ellie demo.

--- examples/src/Ellie.elm
+++ examples/src/Ellie.elm
@@ -1,37 +1,81 @@
 port module Main exposing (main)
 
 import Cli.Option as Option
-import Cli.OptionsParser as OptionsParser
+import Cli.OptionsParser as OptionsParser exposing (with)
+import Cli.OptionsParser.BuilderState as BuilderState
 import Cli.Program as Program
 import Json.Decode exposing (..)
 
 
-type alias GreetOptions =
-    { name : String
-    , maybeGreeting : Maybe String
+type CliOptions
+    = Init
+    | Clone String
+    | Log LogOptions
+
+
+type alias LogOptions =
+    { maybeAuthorPattern : Maybe String
+    , maybeMaxCount : Maybe Int
+    , statisticsMode : Bool
+    , maybeRevisionRange : Maybe String
+    , restArgs : List String
     }
 
 
-programConfig : Program.Config GreetOptions
+programConfig : Program.Config CliOptions
 programConfig =
     Program.config { version = "1.2.3" }
         |> Program.add
-            (OptionsParser.build GreetOptions
-                |> OptionsParser.with (Option.requiredKeywordArg "name")
-                |> OptionsParser.with (Option.optionalKeywordArg "greeting")
+            (OptionsParser.buildSubCommand "init" Init
+                |> OptionsParser.withDoc "initialize a git repository"
             )
+        |> Program.add
+            (OptionsParser.buildSubCommand "clone" Clone
+                |> with (Option.requiredPositionalArg "repository")
+            )
+        |> Program.add (OptionsParser.map Log logOptionsParser)
 
 
-init : Flags -> GreetOptions -> Cmd Never
-init flags { maybeGreeting, name } =
-    maybeGreeting
-        |> Maybe.withDefault "Hello"
-        |> (\greeting -> greeting ++ " " ++ name ++ "!")
+logOptionsParser : OptionsParser.OptionsParser LogOptions BuilderState.NoMoreOptions
+logOptionsParser =
+    OptionsParser.buildSubCommand "log" LogOptions
+        |> with (Option.optionalKeywordArg "author")
+        |> with
+            (Option.optionalKeywordArg "max-count"
+                |> Option.validateMapIfPresent String.toInt
+            )
+        |> with (Option.flag "stat")
+        |> OptionsParser.withOptionalPositionalArg
+            (Option.optionalPositionalArg "revision range")
+        |> OptionsParser.withRestArgs
+            (Option.restArgs "rest args")
+
+
+init : Flags -> CliOptions -> Cmd Never
+init flags cliOptions =
+    (case cliOptions of
+        Init ->
+            "Initialized empty Git repository..."
+
+        Clone url ->
+            "Cloning `" ++ url ++ "`..."
+
+        Log options ->
+            [ "Logging..." |> Just
+            , options.maybeAuthorPattern |> Maybe.map (\authorPattern -> "authorPattern: " ++ authorPattern)
+            , options.maybeMaxCount |> Maybe.map (\maxCount -> "maxCount: " ++ toString maxCount)
+            , "stat: " ++ toString options.statisticsMode |> Just
+            , options.maybeRevisionRange |> Maybe.map (\revisionRange -> "revisionRange: " ++ toString revisionRange)
+            ]
+                |> List.filterMap identity
+                |> String.join "\n"
+    )
         |> print
 
 
 dummy : Decoder String
 dummy =
+    -- this is a workaround for an Elm compiler bug
     Json.Decode.string
 
 
