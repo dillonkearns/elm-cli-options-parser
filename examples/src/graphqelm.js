71130347bca27b962b242284fb016b3acec4ef71
Dillon Kearns <dillon@dillonkearns.com>
2018-08-23 21:22:21 -0700

    Remove unused scripts.

--- examples/src/graphqelm.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env node
-
-let program = require('./elm.js').Graphqelm.worker(process.argv)
-
-program.ports.print.subscribe(message => console.log(message))
--- examples/src/simple.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env node
-
-let program = require('./elm.js').Simple.worker(process.argv)
-
-program.ports.print.subscribe(message => console.log(message))
